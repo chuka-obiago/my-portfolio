90d49b2ec45b040359eee32d2046b48c8c5cb1be
Ekene Obiago <chukaobiago@gmail.com>
2025-09-20 15:17:38 +0100

    Add vercel.json for SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="thankyou" element={<ThankYou/>} />
+        <Route path="/thankyou" element={<ThankYou/>} />
       </Routes>
     </BrowserRouter>
   );
